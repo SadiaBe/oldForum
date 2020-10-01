bf63264823dc341debcca5a4d1afe21fb1e9f542
SadiaBe <sadia.islam.badhon@gmail.com>
2020-09-30 22:59:54 -0400

    added login page

--- forum-frontend/src/components/Login.js
+++ forum-frontend/src/components/Login.js
@@ -6,7 +6,29 @@ class Login extends React.Component {
     render() {
         console.log(this.props)
     return (
-    <div> Login comp </div>
+    <div> Login comp
+      <MuiThemeProvider>
+          <div>
+          <AppBar
+             title="Login"
+           />
+           <TextField
+             hintText="Enter your Username"
+             floatingLabelText="Username"
+             onChange = {(event,newValue) => this.setState({username:newValue})}
+             />
+           <br/>
+             <TextField
+               type="password"
+               hintText="Enter your Password"
+               floatingLabelText="Password"
+               onChange = {(event,newValue) => this.setState({password:newValue})}
+               />
+             <br/>
+             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+         </div>
+         </MuiThemeProvider>
+      </div> </div>
 
   );
 }
