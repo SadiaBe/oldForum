04b95872a3a4597f9395108bac72751b990632ec
SadiaBe <sadia.islam.badhon@gmail.com>
2020-09-30 23:07:14 -0400

    added handle functions

--- forum-frontend/src/components/Login.js
+++ forum-frontend/src/components/Login.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Form } from 'semantic-ui-react';
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { Button, FormGroup, FormControl} from "react-bootstrap";
 
 class Login extends React.Component {
     
     render() {
         console.log(this.props)
+
+
+        function validateForm() {
+          return email.length > 0 && password.length > 0;
+        }
+      
+        function handleSubmit(event) {
+          event.preventDefault();
+        }
     return (
     <div> Login comp
       
