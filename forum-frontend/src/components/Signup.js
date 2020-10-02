bcbffbbbf53c4588dd38c9d28ad834c22b51cab8
SadiaBe <sadia.islam.badhon@gmail.com>
2020-10-02 15:48:15 -0400

    fixed signup and added imports

--- forum-frontend/src/components/Signup.js
+++ forum-frontend/src/components/Signup.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 
 class Signup extends React.Component {
@@ -9,20 +10,10 @@ return (
                 <h3>Sign Up</h3>
 
                 <div className="form-group">
-                    <label>First name</label>
+                    <label>Name</label>
                     <input type="text" className="form-control" placeholder="First name" />
                 </div>
-
-                <div className="form-group">
-                    <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
-                </div>
-
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
-
+              
                 <div className="form-group">
                     <label>Password</label>
                     <input type="password" className="form-control" placeholder="Enter password" />
