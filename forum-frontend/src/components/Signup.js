012cbc42df0d8097a5d7eb5d437aa3891dd50356
SadiaBe <sadia.islam.badhon@gmail.com>
2020-10-06 12:28:48 -0400

    took out signup form

--- forum-frontend/src/components/Signup.js
+++ forum-frontend/src/components/Signup.js
@@ -6,26 +6,7 @@ class Signup extends React.Component {
 render() {
 return (
     <div> Signup comp
-      <form>
-                <h3>Sign Up</h3>
-
-                <div className="form-group">
-                    <label>Name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
-                </div>
-              
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
-
-                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
-                <p className="forgot-password text-right">
-                    Already registered <a href="#">sign in?</a>
-                </p>
-            </form>
-            
-            
+      
              </div>
 
   );
